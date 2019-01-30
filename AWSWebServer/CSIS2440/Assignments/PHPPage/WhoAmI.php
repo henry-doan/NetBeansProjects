1b956ad9a6e630a0668790996c316d5e8cb9eb31
Henry Doan <henrydoan96@gmail.com>
2019-01-30 12:56:54 -0700

    styled form

--- AWSWebServer/CSIS2440/Assignments/PHPPage/WhoAmI.php
+++ AWSWebServer/CSIS2440/Assignments/PHPPage/WhoAmI.php
@@ -2,25 +2,63 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>WhoAmI</title>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Materialize -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
     </head>
     <body>
-        <div id="form_container">
-            <form method="post" action="WhoYouAre.php">
-                <label>Name: </label>
-                <input type="text" name="Name" />
-                <label>Age: </label>
-                <input type="number" name="Age" />
-                <label>Address: </label>
-                <input type="text" name="Address" />
-                <label>State: </label>
-                <input type="text" name="State" />
-                <label>Sex: 
-                F<input type="radio" value="F" name="Gender" checked="true">  M<input type="radio" value="M" name="Gender">
+        <div class="row container">
+            <br />
+            <br />
+            <h1 class="center-align">Who Am I?</h1>
+            <br />
+            <form class="col s12 center-align" method="post" action="WhoYouAre.php">
+              <div class="row">
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">account_circle</i>
+                  <input id="icon_prefix" type="text" name="Name" />
+                  <label for="icon_prefix">Name</label>
+                </div>
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">cake</i>
+                  <input id="age" type="number" name="Age" />
+                  <label for="age">Age</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">location_on</i>
+                  <input id="address" type="text" name="Address" />
+                  <label for="address">Address</label>
+                </div>
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">location_city</i>
+                  <input id="state" type="text" name="State" />
+                  <label for="state">State</label>
+                </div>
+              </div>
+              <p>
+                <label> Sex:
+                  <br />
+                  <input type="radio" value="F" name="Gender" checked>
+                  <span>F</span>
                 </label>
+              </p>
+              <p>
+                <label>
+                  <input type="radio" value="M" name="Gender">
+                  <span>M</span>
+                </label>
+              </p>
+              <br />
+              <input class="waves-effect waves-light btn" type="submit" name="Submit" value="Submit" />
             </form>
-        <?php
-        // put your code here
-        ?>
+          </div>    
+
+        <!-- Materialize JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>         
     </body>
 </html>
--- AWSWebServer/CSIS2440/Assignments/PHPPage/WhoYouAre.php
+++ AWSWebServer/CSIS2440/Assignments/PHPPage/WhoYouAre.php
@@ -1,13 +1,38 @@
 <!DOCTYPE html>
+
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP Page</title>
-    </head>
-    <body>
+<?php
+
+    print_r($_POST);
+
+    $name = htmlentities($_POST['Name']);
+    $name = strtolower($name);
+    $name = ucwords($name);
+    $age = $_POST['Age'];
+    $address = $_POST['Address'];
+    $state = $_POST['State'];
+    $gender = $_POST['Gender'];
+
+    
+    printf("<br><ul><li>name: %s </li><li>age: %d </li><li>address: %s </li><li>state: %s</li><li>sex: %s</li></ul><br>", $name, $age, $address, $state, $gender);
+    
+//    if ($gender == "Male") {
+//        $img = "Ma.jpg'>";
+//    } else {
+//        $img = "Fe.jpg'>";
+//    }
+    
+    $year = date(Y);
+    print("<p>" . $year . "</p>");
+    for($i = 0; $i < $age; $i++){
+        $year -= 1;
+        print("<p>" . $year . "</p>");
+    }
+    
+    $peom = explode("\n", file_get_contents('PostPage.txt'));
         
-        <?php
-        // put your code here
-        ?>
-    </body>
+    for($i = 0; $i < sizeof($peom); $i++) {
+        print($peom[$i] . "<br>");
+    }
+?>
 </html>
