e4b921e4f8a1f6fb7c00e5f764545ddd7588cbe4
Henry Doan <henrydoan96@gmail.com>
2019-02-01 14:28:42 -0700

    finished with php pages

--- AWSWebServer/CSIS2440/Assignments/PHPPage/WhoAmI.php
+++ AWSWebServer/CSIS2440/Assignments/PHPPage/WhoAmI.php
@@ -7,7 +7,6 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <!-- Materialize -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
     </head>
     <body>
         <div class="row container">
--- AWSWebServer/CSIS2440/Assignments/PHPPage/WhoYouAre.php
+++ AWSWebServer/CSIS2440/Assignments/PHPPage/WhoYouAre.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 
 <html>
+    <meta charset="UTF-8">
+        <title>WhoYouAre</title>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Materialize -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <style>
+            .m-bg {
+                background-color: #00B7FD;
+            }
+            
+            .f-bg {
+                background-color: #E889A5;
+            }
+        </style>
 <?php
 
-    print_r($_POST);
+//    print_r($_POST);
 
     $name = htmlentities($_POST['Name']);
     $name = strtolower($name);
@@ -13,14 +28,31 @@
     $state = $_POST['State'];
     $gender = $_POST['Gender'];
 
+    if ($gender == "M") {
+        $color = "m-bg";
+        $img = "src='./img/guy.jpeg' alt='guy' />";
+    } else {
+        $color = "f-bg";
+        $img = "src='./img/lady.jpeg' alt='lady' />";
+    }
     
-    printf("<br><ul><li>name: %s </li><li>age: %d </li><li>address: %s </li><li>state: %s</li><li>sex: %s</li></ul><br>", $name, $age, $address, $state, $gender);
-    
-//    if ($gender == "Male") {
-//        $img = "Ma.jpg'>";
-//    } else {
-//        $img = "Fe.jpg'>";
-//    }
+    printf( "<body class='%s'>"
+        . "<div class='row container'>"
+        . "<br /><br />"
+        . "<h1 class='center-align'>Who You Are</h1>"
+        . "<div class='container'>"
+        . "<div class='row'>"
+            . "<div class='col s8'>"
+                . "<h1>You are</h1>"
+                . "<ul><li>name: %s </li><li>age: %d </li><li>address: %s </li><li>state: %s</li><li>sex: %s</li></ul><br>"
+            . "</div>"
+            . "<div class='col s4'>"
+                . "<img width='300' class='profile-pic' %s"
+            . "</div>"
+        . "</div>"
+        . "<hr/>"
+    . "</div>", $color, $name, $age, $address, $state, $gender, $img);
+
     
     $year = date(Y);
     print("<p>" . $year . "</p>");
@@ -35,4 +67,6 @@
         print($peom[$i] . "<br>");
     }
 ?>
+        </div>
+    </body>
 </html>
