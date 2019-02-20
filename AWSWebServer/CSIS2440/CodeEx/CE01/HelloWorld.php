680cf0425edb86ae23ebaa5f25f3bc271ffc0097
Henry Doan <henrydoan96@gmail.com>
2019-02-20 12:24:20 -0700

    added first day file

---
+++ AWSWebServer/CSIS2440/CodeEx/CE01/HelloWorld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World Initial Code</title>
+</head>
+
+<body>
+  <h3>My first PHP Program</h3>
+    <?php
+      /*
+      * Henry Doan
+      * 22
+      */
+      echo "Hello world!";
+      $greeting = "<p>PHP is FUN!\n</p>";
+      print($greeting);
+      
+    ?>
+</body>
+</html>
