5fbfa88cb2b9297ab19fb2872b9fd419fb0cea31
Henry Doan <henrydoan96@gmail.com>
2019-04-01 11:57:32 -0600

    finished adventure

--- AWSWebServer/CSIS2440/CodeEx/CE11/DataBaseConnection.php
+++ AWSWebServer/CSIS2440/CodeEx/CE11/DataBaseConnection.php
@@ -8,7 +8,7 @@
 	or die ('Could not connect to the database server' . mysqli_connect_error($con));
 
 if ($con->connect_error == FALSE) {
-    echo "<h2>We are Connected</h2>";
+//    echo "<h2>We are Connected</h2>";
 } else {
     echo $con->connect_error;
 }
